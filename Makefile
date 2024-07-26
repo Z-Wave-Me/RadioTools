d1a3f4ceec5030d56d0339f60c749a439291782c
Oleg Nemanov <lego12239@yandex.ru>
2024-07-26 12:08:19 +0300

    add init.d file
    Init.d script with tiny supervisor.
    Install this file with make install command.

--- Makefile
+++ Makefile
@@ -12,6 +12,8 @@ clean:
 	rm -f *~
 
 install: dist
-	mkdir -p $(DESTDIR)
-	unxz -c zme_radiotools.txz | ( cd $(DESTDIR); tar -x --exclude="zme_radiotools.crontab.in" --no-same-owner )
+	mkdir -p $(DESTDIR) /var/cache/zme_radiotools
+	unxz -c zme_radiotools.txz | ( cd $(DESTDIR); tar -x --exclude="zme_radiotools.crontab.in" --exclude="zme_radiotools.initd.in" --no-same-owner )
 	sed -re 's/@ZME_DESTDIR@/$(subst /,\/,$(DESTDIR))/' zme_radiotools.crontab.in > /etc/cron.d/zme_radiotools
+	sed -re 's/@ZME_DESTDIR@/$(subst /,\/,$(DESTDIR))/' zme_radiotools.initd.in > /etc/init.d/zme_radiotools
+	chmod a+rx /etc/init.d/zme_radiotools
---
+++ sv.simple.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Version: 1.0
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Dependencies: nohup, setsid.
+#
+# Usage:
+#   export PIDFILE=/var/run/prog.pid
+#   ./sv.simple.sh start [OTHER_ARGS]
+#   ./sv.simple.sh stop
+
+case "$1" in
+start)
+	(nohup setsid $0 RUN "$@" >/dev/null 2>&1 &)
+	exit
+	;;
+stop)
+	if ! [[ -f "$PIDFILE" ]]; then
+		echo "Already stopped" >&2
+		exit
+	fi
+
+	# start-stop-daemon?
+	kill -TERM -`cat "$PIDFILE"`
+	rm -f "$PIDFILE"
+	exit
+	;;
+RUN)
+	;;
+*)
+	echo "Unknown action: $1" >&2
+	exit 1
+	;;
+esac
+
+shift
+shift
+
+if [ "$PIDFILE" ]; then
+	echo $$ > "$PIDFILE"
+fi
+
+hdl_sigterm()
+{
+	trap - SIGTERM
+	kill -TERM -$$
+}
+
+trap hdl_sigterm SIGTERM
+trap hdl_sigterm SIGINT
+trap hdl_sigterm SIGHUP
+trap hdl_sigterm SIGQUIT
+
+cd `dirname $0`
+
+while true; do
+	# REPLACE THE NEXT LINE WITH YOUR COMMAND
+	# KEEP & at the end
+	$CMD &
+	wait
+	sleep 1s
+done
